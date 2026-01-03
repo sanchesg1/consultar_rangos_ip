fcae2fa233c9135ab179903e9178e1b058b1b304
sanchesg1 <40778411+sanchesg1@users.noreply.github.com>
2026-01-03 00:18:02 -0300

    Add files via upload

---
+++ lista.py
@@ -0,0 +1,37 @@
+import ipaddress
+import datetime
+import os
+
+
+#meter el segmento en 1 txt llamado 'segmentos.txt'
+def generar_lista_ips():
+    archivo_entrada = "segmentos.txt"
+    
+#solo pa validar
+    if not os.path.exists(archivo_entrada):
+        print(f"falta el archivo con las ip")
+        return
+
+#esto tmb porque soy vago y es para hacer los resultados en un archivo txt escalable. los guarda en formato lista_ip_aaaammdd_mmss.txt
+    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    archivo_salida = f"lista_ips_{timestamp}.txt"
+    conteo_total = 0
+    try:
+        with open(archivo_entrada, 'r') as f_in, open(archivo_salida, 'w') as f_out:
+            for linea in f_in:
+                segmento = linea.strip()
+                if not segmento: continue
+                try:
+                    red = ipaddress.ip_network(segmento, strict=False)
+                    for ip in red:
+                        f_out.write(str(ip) + '\n')
+                        conteo_total += 1
+                except ValueError:
+#solo pongo un comentario para saber q termino
+        print(f"fin en {archivo_salida}")
+
+    except Exception as e:
+        print(f"hubo un error jefe {e}")
+
+if __name__ == '__main__':
+    generar_lista_ips()
---
+++ readme.md
@@ -0,0 +1,3 @@
+soy vago, sirve para meter un segmento ip y te devuelve todos las ips del mismo.
+
+puede ser de un /32 hasta un /0 (te va a matar el cpu creo jeje)
